3b2533b7d3cb633c538e450929ad62468e64bcbc
Miyashiro <32512576+miyashiro-kun@users.noreply.github.com>
2025-12-22 02:57:38 +0700

    Update starrail.js

--- api/starrail.js
+++ api/starrail.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const response = await fetch(
-      "https://api.ennead.cc/mihoyo/starrail/news/notices",
+      "https://sg-hyp-api.hoyoverse.com/hyp/hyp-connect/api/getGamePackages?game_ids[]=4ziysqXOQ8&launcher_id=VYTpXlbWo8",
       {
         method: "GET",
         headers: {
